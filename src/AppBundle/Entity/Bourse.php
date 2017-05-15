108d45c45d0f29f61b4a4326044b64a9ecfed5a4
Clément Garbay <clementgarbay@gmail.com>
2017-05-15 14:45:39 +0200

    Bourse entity

--- src/AppBundle/Entity/Bourse.php
+++ src/AppBundle/Entity/Bourse.php
@@ -2,14 +2,24 @@
 
 namespace AppBundle\Entity;
 
+/**
+ * @ApiResource(
+ *     collectionOperations={
+ *         "get"={"method"="GET"},
+ *         "post"={"method"="POST"}
+ *     },
+ *     itemOperations={
+ *         "get"={"method"="GET"}
+ *     })
+ */
 class Bourse
 {
     /**
      * @var string Le nom du bénéficiaire
      * @Assert\NotBlank(message="Le nom du bénéficiaire ne doit pas être vide")
      * @Assert\Type(
-     *     type="string,
-     *     message="Le nom du bénéficiaire doit être une chaine de caractères"
+     *     type="string",
+     *     message="Le nom du bénéficiaire doit être une chaine de caractères valide"
      * )
      */
     protected $beneficiaireNom;
@@ -21,5 +31,53 @@ class Bourse
      */
     protected $beneficiaireEmail;
 
+    /**
+     * @var float Le revenu fiscal de référence
+     * @Assert\NotBlank(message="Le revenu fiscal de référence ne doit pas être vide")
+     * @Assert\Type(
+     *     type="float",
+     *     message="Le revenu fiscal de référence doit être un nombre décimal"
+     * )
+     */
+    protected $revenuFiscalReference;
 
+    /**
+     * @var array La géolocalisation du foyer
+     */
+    protected $geolocFoyer;
+
+    /**
+     * @var array La géolocalisation de l'école
+     */
+    protected $geolocEcole;
+
+    /**
+     * @var int Le nombre de personnes à charge
+     * @Assert\NotBlank(message="Le nombre de personnes à charge ne doit pas être vide")
+     * @Assert\Type(
+     *     type="int",
+     *     message="Le nombre de personnes à charge doit être un nombre entier"
+     * )
+     */
+    protected $nbPersonnesCharge;
+
+    /**
+     * @var float Le montant affecté
+     * @Assert\NotBlank(message="Le montant affecté ne doit pas être vide")
+     * @Assert\Type(
+     *     type="float",
+     *     message="Le montant affecté doit être un nombre décimal"
+     * )
+     */
+    protected $montantAffecte;
+
+    /**
+     * @var string Le statut
+     * @Assert\NotBlank(message="Le statut ne doit pas être vide")
+     * @Assert\Type(
+     *     type="int",
+     *     message="Le statut doit être une chaine de caractères valide"
+     * )
+     */
+    protected $statut;
 }
